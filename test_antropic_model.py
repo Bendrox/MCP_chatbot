de1e53288bb13d019c9075fb632194368df48ac5
Bendrox <oussamabendrisspro@gmail.com>
2025-08-28 11:32:25 +0200

    Adding model - Antropic -  Add test module + setting model param

--- test_antropic_model.py
+++ test_antropic_model.py
@@ -1,16 +1,9 @@
-#!curl https://api.anthropic.com/v1/messages \
-#        --header "x-api-key: REDACTED" \
-#        --header "anthropic-version: 2023-06-01" \
-#        --header "content-type: application/json" \
-#        --data '{"model": "claude-3-7-sonnet-latest", "max_tokens": 1024, "messages": [{"role": "user", "content": "Hello, world"}]}'
-
 from dotenv import load_dotenv
 import os
 import anthropic
 
 load_dotenv()
 api_key = os.getenv("ANTHROPIC_API_KEY")
-
 
 client = anthropic.Anthropic()
 
