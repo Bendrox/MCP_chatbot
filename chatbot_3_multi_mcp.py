7cd942060470bb6772491f7805f960be3f39ad5b
Bendrox <oussamabendrisspro@gmail.com>
2025-08-31 16:28:34 +0200

    Chatbot v3 - Clean version - multi MCP Client + LLM proper integration

---
+++ chatbot_3_multi_mcp.py
@@ -0,0 +1,190 @@
+# New version v3 adding multi client + servers (internal + external): 
+
+# 1 - Instead of having one session, you now have a list of client sessions where each client session establishes a 1-to-1
+# connection to each server;
+
+# 2 - available_tools includes the definitions of all the tools exposed by all servers that the chatbot can connect to.
+
+# 3 - tool_to_session maps the tool name to the corresponding client session; in this way, when the LLM decides on a 
+# particular tool name, you can map it to the correct client session so you can use that session to send tool_call 
+# request to the right MCP server.
+
+# 4 - exit_stack is a context manager that will manage the mcp client objects and their sessions and ensures that they 
+# are properly closed. In v2, we did not use it because you used the with statement which behind the scenes uses 
+# a context manager. Here we may end up using multiple nested with statements 
+# since we have multiple servers to connect to. 
+# exit_stack allows you to dynamically add the mcp clients and their sessions as you'll see in the code below.
+
+# 5 - connect_to_servers reads the server configuration file and for each single server, it calls the helper method 
+# connect_to_server. In this latter method, an MCP client is created and used to launch the server as a sub-process and
+# then a client session is created to connect to the server and get a description of the list of the tools provided by 
+# the server.
+
+# 6 - cleanup is a helper method that ensures all your connections are properly shut down when you're done with them. 
+# In v2, we relied on the with statement to automatically clean up resources. 
+# This cleanup method serves a similar purpose, but for all the resources you've added to your exit_stack; 
+# it closes (your MCP clients and sessions) in the reverse order they were added - like stacking and unstacking plates.
+# This is particularly important in network programming to avoid resource leaks.
+
+    
+from dotenv import load_dotenv
+from anthropic import Anthropic
+from mcp import ClientSession, StdioServerParameters, types
+from mcp.client.stdio import stdio_client
+from typing import List, Dict, TypedDict
+from contextlib import AsyncExitStack
+import json
+import asyncio
+
+from claude_models import Claude4, Claude35
+
+load_dotenv()
+
+claude_model_35 = Claude35()
+
+# params
+max_tokens_param= 350
+
+class ToolDefinition(TypedDict):
+    name: str
+    description: str
+    input_schema: dict
+
+class MCP_ChatBot:
+
+    def __init__(self):
+        # Initialize session and client objects
+        self.sessions: List[ClientSession] = [] # new
+        self.exit_stack = AsyncExitStack() # new
+        self.anthropic = Anthropic()
+        self.available_tools: List[ToolDefinition] = [] # new
+        self.tool_to_session: Dict[str, ClientSession] = {} # new
+
+
+    async def connect_to_server(self, server_name: str, server_config: dict) -> None:
+        """Connect to a single MCP server."""
+        try:
+            server_params = StdioServerParameters(**server_config)
+            stdio_transport = await self.exit_stack.enter_async_context(
+                stdio_client(server_params)
+            ) # new
+            read, write = stdio_transport
+            session = await self.exit_stack.enter_async_context(
+                ClientSession(read, write)
+            ) # new
+            await session.initialize()
+            self.sessions.append(session)
+            
+            # List available tools for this session
+            response = await session.list_tools()
+            tools = response.tools
+            print(f"\nConnected to {server_name} with tools:", [t.name for t in tools])
+            
+            for tool in tools: # new
+                self.tool_to_session[tool.name] = session
+                self.available_tools.append({
+                    "name": tool.name,
+                    "description": tool.description,
+                    "input_schema": tool.inputSchema
+                })
+        except Exception as e:
+            print(f"Failed to connect to {server_name}: {e}")
+
+    async def connect_to_servers(self): # new
+        """Connect to all configured MCP servers."""
+        try:
+            with open("mcp_server_config.json", "r") as file:
+                data = json.load(file)
+            
+            servers = data.get("mcpServers", {})
+            
+            for server_name, server_config in servers.items():
+                await self.connect_to_server(server_name, server_config)
+        except Exception as e:
+            print(f"Error loading server configuration: {e}")
+            raise
+    
+    async def process_query(self, query):
+        messages = [{'role':'user', 'content':query}]
+        response = claude_model_35.generate_with_tools(messages = messages,
+                                                       max_tokens= max_tokens_param,
+                                                       tools = self.available_tools)
+        process_query = True
+        while process_query:
+            assistant_content = []
+            for content in response.content:
+                if content.type =='text':
+                    print(content.text)
+                    assistant_content.append(content)
+                    if(len(response.content) == 1):
+                        process_query= False
+                elif content.type == 'tool_use':
+                    assistant_content.append(content)
+                    messages.append({'role':'assistant', 'content':assistant_content})
+                    tool_id = content.id
+                    tool_args = content.input
+                    tool_name = content.name
+                    
+    
+                    print(f"Calling tool {tool_name} with args {tool_args}")
+                    
+                    # Call a tool
+                    session = self.tool_to_session[tool_name] # new
+                    result = await session.call_tool(tool_name, arguments=tool_args)
+                    messages.append({"role": "user", 
+                                      "content": [
+                                          {
+                                              "type": "tool_result",
+                                              "tool_use_id":tool_id,
+                                              "content": result.content
+                                          }
+                                      ]
+                                    })
+                    response = self.anthropic.messages.create(max_tokens = 2024,
+                                      model = 'claude-3-7-sonnet-20250219', 
+                                      tools = self.available_tools,
+                                      messages = messages) 
+                    
+                    if(len(response.content) == 1 and response.content[0].type == "text"):
+                        print(response.content[0].text)
+                        process_query= False
+
+    
+    
+    async def chat_loop(self):
+        """Run an interactive chat loop"""
+        print("\nMCP Chatbot Started!")
+        print("Type your queries or 'quit' to exit.")
+        
+        while True:
+            try:
+                query = input("\nQuery: ").strip()
+        
+                if query.lower() == 'quit':
+                    break
+                    
+                await self.process_query(query)
+                print("\n")
+                    
+            except Exception as e:
+                print(f"\nError: {str(e)}")
+    
+    async def cleanup(self): # new
+        """Cleanly close all resources using AsyncExitStack."""
+        await self.exit_stack.aclose()
+
+
+async def main():
+    chatbot = MCP_ChatBot()
+    try:
+        # the mcp clients and sessions are not initialized using "with"
+        # like in the previous lesson
+        # so the cleanup should be manually handled
+        await chatbot.connect_to_servers() # new! 
+        await chatbot.chat_loop()
+    finally:
+        await chatbot.cleanup() #new! 
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
